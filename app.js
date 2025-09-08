e191e456418eb7a1dd3c78d98d46bb4881d86d30
rana-ashutosh <ashutoshr033@gmail.com>
2025-09-08 10:18:32 +0530

    learn about JWT and bcrypt methods

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const  postModel = require('./Models/post');
+const  userModel = require('./Models/user');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { default: mongoose } = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+app.post('/register', async (req, res) => {
+    let {email, username, password, age, name } = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send('User already registered');
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                email,
+                name,
+                age,
+                password: hash,
+                posts: [{
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "post"
+                }]
+            });
+
+            let token = jwt.sign({email: email, userid: user._id}, "shhhh");
+            res.cookie('token', token);
+            res.send('registered');
+        });
+    });
+});
+
+app.post('/login', async (req, res) => {
+    let {email, password} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send('Something went wrong');
+
+    bcrypt.compare(password, user.password, function (err, result){
+        if(result) res.status(200).send('You can login');
+        else res.redirect('/login');
+    })
+});
+
+app.get('/logout', (req, res) =>{
+    res.cookie('token', '');
+    res.redirect('/login');
+})
+
+app.listen(3000);
